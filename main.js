39ebadea4bdf9f8cd284eb26fd7e3b8075aa2ae1
vasifismayilov <ismayilov2265@gmail.com>
2022-03-16 11:12:51 -0700

    homework5

---
+++ main.js
@@ -0,0 +1,50 @@
+// const terevezler=['pomidor', 'sogan', 'yerkoku', 'sarimsaq', 'semeni'];
+
+// for (let a=0; a<terevezler.length;) {
+//     let terevez=terevezler[a]
+//     a++
+
+//     if(terevez[0]==="s"){
+//     console.log(terevez);
+//     }
+// }
+
+// ++++++++++++++++++++++++++++++++++++++++++++
+
+// const ofis = ['kitab', 'defter', 'qelem', 'karandas', 'jurnal', 'pozan', 'vereq'];
+
+// for(let a=0; a<ofis.length;) {
+//     const levazimat=ofis[a]
+//     a++
+
+//     if(levazimat.length!==5){
+//         continue
+//     }
+
+//     console.log(levazimat[levazimat.length-1])
+// }
+
+// ++++++++++++++++++++++++++++++++++
+
+
+// for( a=100000;a>0;a--){
+//     if(a%9999===0){
+//         console.log(a);
+//         break
+//     }
+// }
+
+// ++++++++++++++++++++++++++++++
+
+// const computer = {
+//     cpu: 'intel i5',
+//     ram: '8',
+//     gpu: 'NVIDIA',
+// }
+
+// for(let info in computer) {
+//     if(info<3){
+//         continue
+//     }
+//     console.log(info)
+// }
